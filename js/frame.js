ce852c5192810e15145e9e43fec8ab912dab4a2e
dnjswns619 <dnjswns619@naver.com>
2022-12-12 15:27:30 +0900

    file upload, modify

---
+++ js/frame.js
@@ -0,0 +1,266 @@
+(() => {
+  const imgInfo = [
+    {
+      //0
+      values: [
+        "../images/안경테/fde599a4211cc01154d4435957e9d42b.jpg",
+        "../images/안경테/d1cf12a87d191513317e933e3ca06011.jpg",
+        "../images/안경테/e43a71b3d6f536ba6f939a0dc9898732.jpg",
+        "../images/안경테/a9ec1c2e3bf85581559a57b7a007fffe.jpg"
+      ]
+    },
+    {
+      //1
+      values: [
+        "../images/안경테/e27bf12d782676c3725e5d70940a36c3.jpg",
+        "../images/안경테/a9ec1c2e3bf85581559a57b7a007fffe.jpg",
+        "../images/안경테/5c2382f9d06d29864ede8aabed2ebb67.jpg",
+        "../images/안경테/5e99bc0d56a5e5fab520766d969f670c.jpg"
+      ]
+    },
+    {
+      //2
+      values: [
+        "../images/안경테/15db21593eedef3e892ff0d401fdef6f.jpg",
+        "../images/안경테/b9008590f65432243536a4e9f17be046.jpg",
+        "../images/안경테/d7b01c3d05c5cdbb6b6a7baa1a9d5163.jpg",
+        "../images/안경테/e857b9aeaa7d229b930d8baf8e053513.jpg"
+      ]
+    },
+    {
+      //3
+      values: [
+        "../images/안경테/6d949100e4b636080c003a959be47587.jpg",
+        "../images/안경테/f5085b164d21371cea85169b6eae0d56.jpg",
+        "../images/안경테/6af1c26da047ec51ee270c5edccf8804.jpg",
+        "../images/안경테/81efecd6fea3463f5d01ce41f329c7e9.jpg"
+      ]
+    },
+    {
+      //4
+      values: [
+        "../images/안경테/f261b26629c8d9bd2f7a0105df346630.jpg",
+        "../images/안경테/25a5ed11da248e017d294ff20718c61a.jpg",
+        "../images/안경테/136ec87395692d9efd279e20f4e69212.jpg",
+        "../images/안경테/61174804a9f1341f7f11b4b704eb0779.jpg"
+      ]
+    },
+    {
+      //5
+      values: [
+        "../images/안경테/b42a6ba46f4079bcd164b3188beb76b0.jpg",
+        "../images/안경테/e78e8099ed2d39994535e01502fdd81e (1).jpg",
+        "../images/안경테/4fe17a57b2d2097ec26c19ac5ecd4263.jpg",
+        "../images/안경테/27662598a9ce0f08abcb670dd6d836d0.jpg"
+      ]
+    },
+    {
+      //6
+      values: [
+        "../images/안경테/ee4c766cd927d8bb93efa827206f9794 (1).jpg",
+        "../images/안경테/a625bb3762beee6a9f4894eaec3285e0.jpg",
+        "../images/안경테/a5d053c39446a65d3f04dedc1222ce2f.jpg",
+        "../images/안경테/ea9d79df1b010b26f281317910ca9eb0.jpg",
+        "../images/안경테/de266f77a4d990473030d078ee4169ca.jpg"
+      ]
+    },
+    {
+      //7
+      values: [
+        "../images/안경테/ad8b4fffd874b10be971cb4800ebe12b.jpg",
+        "../images/안경테/fde308cad7b93c6400364524aa871683.jpg",
+        "../images/안경테/d9d2ecf60376917364de5bb97f174502 (1).jpg",
+        "../images/안경테/94af0a259bb7e71000540fb9f58a44cd.jpg",
+        "../images/안경테/fd2d14c308082dc0db8aaf02c45eecb7.jpg"
+      ]
+    },
+    {
+      //8
+      values: [
+        "../images/안경테/e357bb0f99f7fa1db24359876c9a6cc0 (1).jpg",
+        "../images/안경테/e357bb0f99f7fa1db24359876c9a6cc0 (2).jpg",
+        "../images/안경테/63417cae60ea9fafbe7836345af9fb76.jpg",
+        "../images/안경테/f8ede178f37318dc78192fc6be7cc0e2.jpg",
+        "../images/안경테/f52c84c2b7b3df9954667cb0afda8977.jpg"
+      ]
+    },
+    {
+      //9
+      values: [
+        "../images/안경테/91c94cdcf42d2be0d38114a67d5612ec.jpg",
+        "../images/안경테/e2000aca6021b9d155f06945279a5c47.jpg",
+        "../images/안경테/545bad096e89e3ca2690c83a58d87e27.jpg",
+        "../images/안경테/02bbf4ece95539609d16213edec59595 (1).jpg",
+        "../images/안경테/120716bb6d5880d09fc39c200ae61e52.jpg"
+      ]
+    },
+    {
+      //10
+      values: [
+        "../images/안경테/70bff802d1e68107a605128d08a09a91.jpg",
+        "../images/안경테/d1a159d24b67888f68ecba253f966624.jpg",
+        "../images/안경테/07ac49a3e0460746a510174c9c8f11bd.jpg",
+        "../images/안경테/3d56778621662305c2ec5eb205d948b2.jpg",
+        "../images/안경테/e378b32de8bcf27cf05654f0a50daeca (1).jpg"
+      ]
+    },
+    {
+      //11
+      values: [
+        "../images/안경테/7954325b77f2732a833037a5c8d569ef.jpg",
+        "../images/안경테/aa1fb359a81efdcd2f5a37a9049541e1.jpg",
+        "../images/안경테/c4553610fea9e19eec0efee235997593.jpg",
+        "../images/안경테/19975fa3ae07e597c8ce12402b67434d.jpg",
+        "../images/안경테/b2f8688247731e210c40d140d9e1b41c.jpg",
+        "../images/안경테/88f62193c04826880a058eeaa23085e0 (1).jpg",
+        "../images/안경테/3c4fc36600cf89d99c32b298d641d4bf.jpg",
+      ]
+    },
+    {
+      //12
+      values: [
+        "../images/안경테/49663b8fb02f174b5db5e3d10b3ae5de.jpg",
+        "../images/안경테/467fcb212e3cca3ccc08bbf170575db6.jpg",
+        "../images/안경테/000b9583cf1b3c7c4071d2262c01382b (1).jpg",
+        "../images/안경테/13b2f7f68517be6061338bae9f45d8b7.jpg"
+      ]
+    },
+    {
+      //13
+      values: [
+        "../images/안경테/c8a3158670350cb308fcab2517373902.jpg",
+        "../images/안경테/e89f785eb76cb74f614f9c783bd321af (1).jpg",
+        "../images/안경테/483c3750454200fde8ef1bb7abe098b7.jpg",
+        "../images/안경테/2c58bb607e47561e809228756236c232.jpg"
+      ]
+    },
+    {
+      //14
+      values: [
+        "../images/안경테/c41b320a238c13dcae4033eb54ce9bae.jpg",
+        "../images/안경테/13e7e1ca2ccb3ada8e917dd3de50cbeb.jpg",
+        "../images/안경테/f9969841cb7d606d00552c323a9ece36 (1).jpg",
+        "../images/안경테/ddaab134de3b1da846c7d3b717406f80.jpg"
+      ]
+    },
+    {
+      //15
+      values: [
+        "../images/안경테/d6cc3def8cbddd9e7f0d8a6810441786.jpg",
+        "../images/안경테/df252fd72d2eeb92148bb1bd7edf22dc.jpg",
+        "../images/안경테/48ad9a341fca79d5f671e6b1684153c6.jpg",
+        "../images/안경테/5ab54a4260a0e85b7d3e18b554046406 (1).jpg",
+        "../images/안경테/f47f01d11832aec8ee2e58f54fbf4727.jpg"
+      ]
+    },
+    {
+      //16
+      values: [
+        "../images/안경테/96418b8350207f7f638d02db6ff5143f.jpg",
+        "../images/안경테/1db8a48ead435a0a5aa3e583feb4c435.jpg",
+        "../images/안경테/a61d1d7b3be7eec8c4cc6e8a388fdad8.jpg",
+        "../images/안경테/889ecc922c03c5c9c15f3dd9b4ba9455 (1).jpg"
+      ]
+    },
+    {
+      //17
+      values: [
+        "../images/안경테/84474b2241ce3a34c4cd8d7f1640f66d.jpg",
+        "../images/안경테/5e19e73de1721e4d4fa9fe71c632d054.jpg",
+        "../images/안경테/d149e85d720cee9d9a46cc3d9c530992 (1).jpg",
+        "../images/안경테/5398ef62e2e65f7c0be8b91af7145c31.jpg"
+      ]
+    },
+    {
+      //18
+      values: [
+        "../images/안경테/d210a138dcec4d7f4e6182c25c5bd936 (1).jpg",
+        "../images/안경테/aa4c22723f41bc789837980a79362b38.jpg"
+      ]
+    }
+  ];
+  
+  for (let e = 0; e < imgInfo.length; e++) {
+    for (let i = 0; i < imgInfo[e].values.length; i++) {
+      document.querySelector(`.item${e}-${i}`).addEventListener('mouseover', () => {
+        document.querySelector(`.item${e}`).src = imgInfo[e].values[i];
+      })
+    }
+  }
+
+
+  let categoryBox = document.querySelectorAll('.category__item');
+  let categoryAll = document.querySelector('.category__all');
+  let categoryMetal = document.querySelector('.category__metal');
+  let categoryAcetate = document.querySelector('.category__acetate');
+  let categoryCombination = document.querySelector('.category__combination');
+  let categoryRefined = document.querySelector('.category__refined');
+  let categoryVintage = document.querySelector('.category__vintage');
+  let item = document.querySelectorAll('.item');
+  let block = document.querySelectorAll('.block');
+  let metal = document.querySelectorAll('.metal');
+  let acetate = document.querySelectorAll('.acetate');
+  let combination = document.querySelectorAll('.combination');
+  let refined = document.querySelectorAll('.refined');
+  let vintage = document.querySelectorAll('.vintage');
+  
+  for (let i = 0; i < 6; i++) {
+    categoryBox[i].addEventListener('click', (e) => {
+      e.stopPropagation();
+      document.querySelector('.current-page').classList.remove('current-page');
+      e.target.classList.add('current-page');
+    })
+  }
+  categoryAll.addEventListener('click', () => {
+    for (let i = 0; i < item.length; i++) {
+      item[i].classList.add('block');
+    }
+    document.querySelector('.glasses__count').innerText = `전체 (${item.length})`;
+  })
+  categoryMetal.addEventListener('click', () => {
+    for (let i = 0; i < block.length; i++) {
+      block[i].classList.remove('block');
+    }
+    for (let i = 0; i < metal.length; i++) {
+      metal[i].classList.add('block');
+    }
+    document.querySelector('.glasses__count').innerText = `메탈 (${metal.length})`;
+  })
+  categoryAcetate.addEventListener('click', () => {
+    for (let i = 0; i < block.length; i++) {
+      block[i].classList.remove('block');
+    }
+    for (let i = 0; i < acetate.length; i++) {
+      acetate[i].classList.add('block');
+    }
+    document.querySelector('.glasses__count').innerText = `아세테이트 (${acetate.length})`;
+  })
+  categoryCombination.addEventListener('click', () => {
+    for (let i = 0; i < block.length; i++) {
+      block[i].classList.remove('block');
+    }
+    for (let i = 0; i < combination.length; i++) {
+      combination[i].classList.add('block');
+    }
+    document.querySelector('.glasses__count').innerText = `콤비네이션 (${combination.length})`;
+  })
+  categoryRefined.addEventListener('click', () => {
+    for (let i = 0; i < block.length; i++) {
+      block[i].classList.remove('block');
+    }
+    for (let i = 0; i < refined.length; i++) {
+      refined[i].classList.add('block');
+    }
+    document.querySelector('.glasses__count').innerText = `리파인드 (${refined.length})`;
+  })
+  categoryVintage.addEventListener('click', () => {
+    for (let i = 0; i < block.length; i++) {
+      block[i].classList.remove('block');
+    }
+    for (let i = 0; i < vintage.length; i++) {
+      vintage[i].classList.add('block');
+    }
+    document.querySelector('.glasses__count').innerText = `빈티지 (${vintage.length})`;
+  })
+  
+})();
